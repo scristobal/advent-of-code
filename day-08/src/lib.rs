8e8e515dfdff768b9afd880a47ef9cbc25e4b586
Samuel Cristobal <el.tobal@gmail.com>
2022-12-08 19:07:40 +0100

    refactor day 8

--- day-08/src/lib.rs
+++ day-08/src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::cmp;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 struct Tree {
@@ -6,234 +6,96 @@ struct Tree {
     size: u32,
 }
 
+fn row(m: Vec<u32>, i: usize) -> (Vec<u32>, Vec<u32>) {
+    let size = (m.len() as f32).sqrt() as usize;
+    let start = i - (i % size);
+
+    let left = m.iter().skip(start).take(i % size).copied().rev().collect();
+    let right = m
+        .iter()
+        .skip(start + i % size + 1)
+        .take(size - 1 - i % size)
+        .copied()
+        .collect();
+
+    (left, right)
+}
+
+fn col(m: Vec<u32>, i: usize) -> (Vec<u32>, Vec<u32>) {
+    let size = (m.len() as f32).sqrt() as usize;
+    let start = i % size;
+
+    let up = m
+        .iter()
+        .skip(start)
+        .step_by(size)
+        .take(i / size)
+        .rev()
+        .copied()
+        .collect();
+
+    let down = m
+        .iter()
+        .skip(i + size)
+        .step_by(size)
+        .take(size - i / size)
+        .copied()
+        .collect();
+
+    (up, down)
+}
+
 pub fn solve_part1(input: &str) -> String {
     let field = input
         .replace("\n", "")
         .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect::<Vec<_>>();
+
+    field
+        .iter()
         .enumerate()
-        .map(|(i, c)| Tree {
-            index: i,
-            size: c.to_digit(10).unwrap(),
+        .filter(|&(t, size)| {
+            let (left, right) = row(field.clone(), t);
+            let (up, down) = col(field.clone(), t);
+
+            let vis_up = up.iter().filter(|&t| t < size).count() == up.len();
+            let vis_down = down.iter().filter(|&t| t < size).count() == down.len();
+            let vis_left = left.iter().filter(|&t| t < size).count() == left.len();
+            let vis_right = right.iter().filter(|&t| t < size).count() == right.len();
+
+            vis_up || vis_down || vis_left || vis_right
         })
-        .collect::<Vec<Tree>>();
-
-    fn row(m: &Vec<Tree>, ind: usize) -> Vec<Tree> {
-        let size = (m.len() as f32).sqrt() as usize;
-        m.iter()
-            .copied()
-            .skip(size * ind)
-            .take(size)
-            .collect::<Vec<_>>()
-    }
-
-    fn col(m: &Vec<Tree>, ind: usize) -> Vec<Tree> {
-        let size = (m.len() as f32).sqrt() as usize;
-        m.iter()
-            .copied()
-            .skip(ind)
-            .step_by(size)
-            .take(size)
-            .collect::<Vec<_>>()
-    }
-
-    fn visible(l: &Vec<Tree>) -> Vec<Tree> {
-        l.iter()
-            .scan(0, |max, &tree| {
-                if tree.size + 1 > *max {
-                    *max = tree.size + 1;
-                    Some((tree, true))
-                } else {
-                    Some((tree, false))
-                }
-            })
-            .filter_map(|(tree, visible)| if visible { Some(tree) } else { None })
-            .collect()
-    }
-
-    fn visible_from_north(m: &Vec<Tree>) -> HashSet<Tree> {
-        let size = (m.len() as f32).sqrt() as usize;
-
-        (0..size)
-            .flat_map(|col_id| {
-                let column = col(&m, col_id);
-
-                visible(&column)
-            })
-            .collect()
-    }
-
-    fn visible_from_south(m: &Vec<Tree>) -> HashSet<Tree> {
-        let size = (m.len() as f32).sqrt() as usize;
-
-        (0..size)
-            .flat_map(|col_id| {
-                let mut column = col(&m, col_id);
-
-                column.reverse();
-
-                visible(&column)
-            })
-            .collect()
-    }
-
-    fn visible_from_east(m: &Vec<Tree>) -> HashSet<Tree> {
-        let size = (m.len() as f32).sqrt() as usize;
-
-        (0..size)
-            .flat_map(|col_id| {
-                let row = row(&m, col_id);
-
-                visible(&row)
-            })
-            .collect()
-    }
-
-    fn visible_from_west(m: &Vec<Tree>) -> HashSet<Tree> {
-        let size = (m.len() as f32).sqrt() as usize;
-
-        (0..size)
-            .flat_map(|col_id| {
-                let mut row = row(&m, col_id);
-
-                row.reverse();
-
-                visible(&row)
-            })
-            .collect()
-    }
-
-    let n = visible_from_north(&field);
-    let s = visible_from_south(&field);
-    let e = visible_from_east(&field);
-    let w = visible_from_west(&field);
-
-    let h = n.union(&s).copied().collect::<HashSet<_>>();
-    let v = e.union(&w).copied().collect::<HashSet<_>>();
-
-    let t = h.union(&v).copied().collect::<HashSet<_>>();
-
-    t.len().to_string()
+        .count()
+        .to_string()
 }
-
 pub fn solve_part2(input: &str) -> String {
     let field = input
         .replace("\n", "")
         .chars()
-        .enumerate()
-        .map(|(i, c)| Tree {
-            index: i,
-            size: c.to_digit(10).unwrap(),
-        })
-        .collect::<Vec<Tree>>();
-
-    fn row(m: &Vec<Tree>, ind: usize) -> Vec<Tree> {
-        let size = (m.len() as f32).sqrt() as usize;
-        m.iter()
-            .copied()
-            .skip(size * ind)
-            .take(size)
-            .collect::<Vec<_>>()
-    }
-
-    //dbg!(row(&field, 2));
-
-    fn col(m: &Vec<Tree>, ind: usize) -> Vec<Tree> {
-        let size = (m.len() as f32).sqrt() as usize;
-        m.iter()
-            .copied()
-            .skip(ind)
-            .step_by(size)
-            .take(size)
-            .collect::<Vec<_>>()
-    }
-
-    fn scenic_score(l: &Vec<Tree>) -> HashMap<Tree, usize> {
-        let rem = l.clone();
-
-        l.iter()
-            .scan(rem, |rem, tree| {
-                let size = tree.size;
-                rem.drain(0..1);
-
-                let mut num_visible_trees = rem
-                    .iter()
-                    .clone()
-                    .take_while(|&tree| tree.size < size)
-                    .count();
-
-                if num_visible_trees != rem.len() {
-                    num_visible_trees += 1;
-                }
-
-                Some((*tree, num_visible_trees))
-            })
-            .collect()
-    }
-
-    fn scenic_score_from_north(m: &Vec<Tree>) -> HashMap<Tree, usize> {
-        let size = (m.len() as f32).sqrt() as usize;
-
-        (0..size)
-            .flat_map(|col_id| {
-                let column = col(&m, col_id);
-
-                scenic_score(&column)
-            })
-            .collect()
-    }
-
-    fn scenic_score_from_south(m: &Vec<Tree>) -> HashMap<Tree, usize> {
-        let size = (m.len() as f32).sqrt() as usize;
-
-        (0..size)
-            .flat_map(|col_id| {
-                let mut column = col(&m, col_id);
-
-                column.reverse();
-
-                scenic_score(&column)
-            })
-            .collect()
-    }
-
-    fn scenic_score_from_east(m: &Vec<Tree>) -> HashMap<Tree, usize> {
-        let size = (m.len() as f32).sqrt() as usize;
-
-        (0..size)
-            .flat_map(|col_id| {
-                let row = row(&m, col_id);
-
-                scenic_score(&row)
-            })
-            .collect()
-    }
-
-    fn scenic_score_from_west(m: &Vec<Tree>) -> HashMap<Tree, usize> {
-        let size = (m.len() as f32).sqrt() as usize;
-
-        (0..size)
-            .flat_map(|col_id| {
-                let mut row = row(&m, col_id);
-
-                row.reverse();
-
-                scenic_score(&row)
-            })
-            .collect()
-    }
-
-    let n = scenic_score_from_north(&field);
-    let s = scenic_score_from_south(&field);
-    let e = scenic_score_from_east(&field);
-    let w = scenic_score_from_west(&field);
+        .map(|c| c.to_digit(10).unwrap())
+        .collect::<Vec<_>>();
 
     field
         .iter()
-        .map(|tree| {
-            n.get(tree).unwrap()
-                * s.get(tree).unwrap()
-                * w.get(tree).unwrap()
-                * e.get(tree).unwrap()
+        .enumerate()
+        .map(|(t, size)| {
+            let (left, right) = row(field.clone(), t);
+            let (up, down) = col(field.clone(), t);
+
+            let score_up = up.iter().take_while(|&t| t < size).count() + 1;
+            let score_up = cmp::min(score_up, up.len());
+
+            let score_down = down.iter().take_while(|&t| t < size).count() + 1;
+            let score_down = cmp::min(score_down, down.len());
+
+            let score_left = left.iter().take_while(|&t| t < size).count() + 1;
+            let score_left = cmp::min(score_left, left.len());
+
+            let score_right = right.iter().take_while(|&t| t < size).count() + 1;
+            let score_right = cmp::min(score_right, right.len());
+
+            score_up * score_down * score_left * score_right
         })
         .max()
         .unwrap()
