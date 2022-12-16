a912f371daeae3ea427d28b65562c1fb77e57db0
Samuel Cristobal <el.tobal@gmail.com>
2022-12-16 21:07:47 +0100

    day 16 part 2, quick and dirty

--- day-16/src/lib.rs
+++ day-16/src/lib.rs
@@ -1,4 +1,3 @@
-use core::time;
 use std::collections::{HashMap, HashSet};
 
 use nom::character::complete::alpha1;
@@ -8,14 +7,11 @@ use nom::{
     branch::alt,
     bytes::complete::tag,
     character::complete::{self, newline},
-    multi::{separated_list0, separated_list1},
-    sequence::{delimited, separated_pair},
-    *,
+    multi::separated_list0,
 };
 
-use petgraph::algo::{dijkstra, min_spanning_tree};
+use petgraph::algo::dijkstra;
 
-use petgraph::dot::{Config, Dot};
 use petgraph::Graph;
 
 use petgraph::graph::NodeIndex;
@@ -31,6 +27,20 @@ struct State<'a> {
     time_left: i32,
     flow: i32,
     path: Vec<&'a NodeIndex>,
+    options: Vec<&'a NodeIndex>,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+struct SingleState<'a> {
+    time_left: i32,
+    flow: i32,
+    path: Vec<&'a NodeIndex>,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+struct PairState<'a> {
+    me: SingleState<'a>,
+    elephant: SingleState<'a>,
     options: Vec<&'a NodeIndex>,
 }
 
@@ -64,7 +74,7 @@ fn file(s: &str) -> IResult<&str, Vec<(Node, Vec<String>)>> {
 }
 
 pub fn solve_part1(input: &str) -> String {
-    let (s, raw) = file(input).unwrap();
+    let (_, raw) = file(input).unwrap();
 
     let edges = raw
         .iter()
@@ -190,8 +200,194 @@ pub fn solve_part1(input: &str) -> String {
 }
 
 pub fn solve_part2(input: &str) -> String {
-    dbg!(input);
-    todo!()
+    let (_, raw) = file(input).unwrap();
+
+    let edges = raw
+        .iter()
+        .map(|(origin, destinations)| (origin.name.clone(), destinations.clone()))
+        .collect::<HashMap<_, _>>();
+
+    let valves = raw
+        .iter()
+        .filter_map(|(node, _)| {
+            if node.rate > 0 {
+                Some((node.name.clone(), node.rate))
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
+
+    let mut g = Graph::<Node, ()>::new();
+
+    let node_index = raw
+        .into_iter()
+        .map(|(node, _)| {
+            let name = node.name.clone();
+            let node_index = g.add_node(node);
+            (name, node_index)
+        })
+        .collect::<HashMap<_, _>>();
+
+    for (name, index) in &node_index {
+        let dests = edges.get(name).unwrap();
+
+        for dest in dests {
+            let index_2 = node_index.get(dest).unwrap();
+
+            g.add_edge(*index, *index_2, ());
+        }
+    }
+
+    let mut distances = HashMap::new();
+
+    for a in node_index.values() {
+        let dist_map = dijkstra(&g, *a, None, |_| 1);
+
+        for (b, dist) in dist_map {
+            distances.insert((*a, b), dist);
+        }
+    }
+
+    let mut indexes = valves
+        .iter()
+        .map(|(name, _)| node_index.get(name).unwrap())
+        .rev()
+        .collect::<Vec<_>>();
+
+    let rates = valves
+        .iter()
+        .map(|(name, rate)| (node_index.get(name).unwrap(), rate))
+        .collect::<HashMap<_, _>>();
+
+    indexes.sort_unstable_by(|a, b| {
+        let a_rate = *rates.get(a).unwrap();
+        let b_rate = *rates.get(b).unwrap();
+
+        b_rate.cmp(a_rate)
+    });
+
+    let start = node_index.get("AA").unwrap();
+
+    let initial = SingleState {
+        time_left: 26,
+        flow: 0,
+        path: vec![start],
+    };
+
+    let initial = PairState {
+        me: initial.clone(),
+        elephant: initial.clone(),
+        options: indexes,
+    };
+
+    let mut discovered: HashSet<PairState> = HashSet::new();
+
+    let mut stack = Vec::new();
+    stack.push(initial);
+
+    let mut max_flow = 0;
+    let mut count = 0;
+
+    while let Some(state) = stack.pop() {
+        if !discovered.contains(&state) {
+            for next in &state.options {
+                let rate = *rates.get(next).unwrap();
+
+                let me_node = *state.me.path.last().unwrap();
+
+                let me_time_left =
+                    (state.me.time_left - distances.get(&(*me_node, **next)).unwrap() - 1).max(0);
+
+                let me_flow = state.me.flow + rate * me_time_left;
+                let mut me_path = state.me.path.clone();
+                me_path.push(next);
+
+                let elephant_node = *state.elephant.path.last().unwrap();
+
+                let elephant_time_left = (state.elephant.time_left
+                    - distances.get(&(*elephant_node, **next)).unwrap()
+                    - 1)
+                .max(0);
+
+                let elephant_flow = state.elephant.flow + rate * elephant_time_left;
+                let mut elephant_path = state.elephant.path.clone();
+                elephant_path.push(next);
+
+                let mut options = state.options.clone();
+
+                options.retain(|option| {
+                    if option != next {
+                        return true;
+                    }
+
+                    let d = *distances.get(&(**next, **option)).unwrap();
+
+                    d >= (me_time_left.max(elephant_time_left) + 1)
+                });
+
+                if options.is_empty() || (me_time_left == 0 && elephant_time_left == 0) {
+                    count += 1;
+
+                    let both_flow =
+                        (me_flow + state.elephant.flow).max(elephant_flow + state.me.flow);
+
+                    if max_flow < both_flow {
+                        max_flow = both_flow;
+                        println!("new max {} after {}", &max_flow, &count);
+                    }
+                } else {
+                    let me_state = SingleState {
+                        time_left: me_time_left,
+                        flow: me_flow,
+                        path: me_path,
+                    };
+
+                    let elephant_state = SingleState {
+                        time_left: elephant_time_left,
+                        flow: elephant_flow,
+                        path: elephant_path,
+                    };
+
+                    if elephant_time_left > me_time_left {
+                        if elephant_time_left > 0 {
+                            stack.push(PairState {
+                                me: state.me.clone(),
+                                elephant: elephant_state,
+                                options: options.clone(),
+                            });
+                        }
+
+                        if me_time_left > 0 {
+                            stack.push(PairState {
+                                me: me_state,
+                                elephant: state.elephant.clone(),
+                                options: options.clone(),
+                            });
+                        }
+                    } else {
+                        if me_time_left > 0 {
+                            stack.push(PairState {
+                                me: me_state,
+                                elephant: state.elephant.clone(),
+                                options: options.clone(),
+                            });
+                        }
+                        if elephant_time_left > 0 {
+                            stack.push(PairState {
+                                me: state.me.clone(),
+                                elephant: elephant_state,
+                                options: options.clone(),
+                            });
+                        }
+                    }
+                }
+            }
+        };
+        discovered.insert(state);
+    }
+
+    max_flow.to_string()
 }
 
 #[cfg(test)]
@@ -206,10 +402,9 @@ mod tests {
         assert_eq!(result, "1651");
     }
 
-    #[ignore = "not implemented"]
     #[test]
     fn part2_works() {
         let result = solve_part2(INPUT);
-        assert_eq!(result, "");
+        assert_eq!(result, "1707");
     }
 }
