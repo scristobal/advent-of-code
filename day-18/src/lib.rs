f6047d0f3af6d07877686b19ccb327473d19e52c
Samuel Cristobal <el.tobal@gmail.com>
2022-12-18 18:10:43 +0100

    day 18, part 1 simpler

--- day-18/src/lib.rs
+++ day-18/src/lib.rs
@@ -17,22 +17,21 @@ fn parse(s: &str) -> HashSet<[i32; 3]> {
 pub fn solve_part1(input: &str) -> String {
     let cubes = parse(input);
 
-    let mut surface = 0;
+    let faces_between_blocks = cubes
+        .iter()
+        .map(|cube| {
+            (0..3_usize)
+                .cartesian_product([-1, 1].into_iter())
+                .filter(|(dim, dir)| {
+                    let mut adjacent = *cube;
+                    adjacent[*dim] += dir;
+                    cubes.contains(&adjacent)
+                })
+                .count()
+        })
+        .sum::<usize>();
 
-    for cube in &cubes {
-        surface += 6;
-
-        for (dim, dir) in (0..3).cartesian_product([-1, 1].into_iter()) {
-            let mut adjacent = *cube;
-
-            adjacent[dim] += dir;
-            if cubes.contains(&adjacent) {
-                surface -= 1;
-            }
-        }
-    }
-
-    surface.to_string()
+    (6 * cubes.len() - faces_between_blocks).to_string()
 }
 
 pub fn solve_part2(input: &str) -> String {
@@ -48,37 +47,34 @@ pub fn solve_part2(input: &str) -> String {
     let mut surface = 0; // outside faces, algorithm output
 
     while let Some(face) = stack.pop() {
-        if !visited.contains(&face) {
-            let (cube, face_dim, face_dir) = face;
-
-            surface += 1; // warranted to be facing outside, see (1) (2) and (3)
-
-            for (dim, dir) in (0..3).cartesian_product([-1, 1].into_iter()) {
-                if dim == face_dim {
-                    continue; // impossible to rotate on self dimension, not necessary
-                }
-
-                let mut diagonal = cube;
-                diagonal[face_dim] += face_dir;
-                diagonal[dim] += dir;
-
-                if cubes.contains(&diagonal) {
-                    stack.push((diagonal, dim, -dir)); // (1) if diagonal cube in set, faces are connected
-                    continue;
-                }
-
-                let mut adjunct = cube;
-                adjunct[dim] += dir; //
-
-                if cubes.contains(&adjunct) {
-                    stack.push((adjunct, face_dim, face_dir)); // (2) if diagonal cube not in set, but adjacent is, faces are connected
-                    continue;
-                }
-
-                stack.push((cube, dim, dir)); // (3) no diagonal nor adjacent in set, contiguous face is connected
-            }
+        if !visited.insert(face) {
+            continue;
         }
-        visited.insert(face);
+
+        let (cube, face_dim, face_dir) = face;
+
+        surface += 1; // warrantied to be facing outside, see (1) (2) and (3)
+
+        for (dim, dir) in (0..3).cartesian_product([-1, 1].into_iter()) {
+            let mut diagonal = cube;
+            diagonal[face_dim] += face_dir;
+            diagonal[dim] += dir;
+
+            if cubes.contains(&diagonal) {
+                stack.push((diagonal, dim, -dir)); // (1) if diagonal cube in set, faces are connected
+                continue;
+            }
+
+            let mut adjunct = cube;
+            adjunct[dim] += dir; //
+
+            if cubes.contains(&adjunct) {
+                stack.push((adjunct, face_dim, face_dir)); // (2) if diagonal cube not in set, but adjacent is, faces are connected
+                continue;
+            }
+
+            stack.push((cube, dim, dir)); // (3) no diagonal nor adjacent in set, contiguous face is connected
+        }
     }
 
     surface.to_string()
