247df797398e6bca2e5df5f1b6539ff4773118b5
Samuel Cristobal <el.tobal@gmail.com>
2023-12-03 11:02:37 +0100

    Day 3, 2023. Working but dirty

---
+++ 2023/day-03/src/bin/part-2.rs
@@ -0,0 +1,29 @@
+/**
+ * Advent of code solutions
+ * by Samuel Cristobal github.com/scristobal/advent-of-code
+ * Licensed under MIT
+ */
+use advent_of_code::solve_part2;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = include_str!("../../input.txt");
+    Ok(println!("{}", solve_part2(input)?))
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    const INPUT: &str = include_str!("../../sample-part-2.txt");
+    const ANSWER: &str = "467835";
+
+    #[test]
+    fn part2_works() {
+        let result = solve_part2(INPUT).unwrap();
+        assert!(!INPUT.is_empty(), "is sample-part-2.txt empty?");
+        assert!(!ANSWER.is_empty(), "provide a sample answer");
+        assert_eq!(result, ANSWER);
+    }
+}
---
+++ 2023/day-03/src/lib.rs
@@ -0,0 +1,185 @@
+/*
+ * Advent of code solutions
+ * by Samuel Cristobal
+ * https://github.com/scristobal/advent-of-code
+ * Licensed under MIT, 2023
+ */
+
+#[derive(PartialEq, Debug)]
+struct Number {
+    value: u32,
+    start: usize,
+    end: usize,
+}
+
+type Numbers = Vec<Number>;
+type Symbols = Vec<usize>;
+
+fn parse_input(input: &str) -> (Numbers, Symbols) {
+    let res = input.chars().enumerate().fold(
+        (vec![], vec![], None::<Number>),
+        |(mut numbers, mut symbols, mut partial_number), (i, c)| {
+            if c.is_numeric() {
+                let digit = c.to_digit(10).unwrap();
+                match partial_number {
+                    Some(mut number) => {
+                        let value = digit + number.value * 10;
+                        number.value = value;
+                        partial_number = Some(number);
+                    }
+                    None => {
+                        partial_number = Some(Number {
+                            value: digit,
+                            start: i,
+                            end: i,
+                        });
+                    }
+                }
+            } else {
+                if let Some(mut number) = partial_number {
+                    number.end = i - 1;
+                    numbers.push(number);
+                    partial_number = None;
+                }
+                if c != '.' {
+                    symbols.push(i);
+                }
+            }
+            (numbers, symbols, partial_number)
+        },
+    );
+    (res.0, res.1)
+}
+
+fn adjacent(width: usize) -> Box<dyn Fn(usize) -> Vec<usize>> {
+    Box::new(move |i| {
+        let mut res = vec![];
+
+        // cross
+        if i % width != 0 {
+            res.push(i - 1);
+        }
+        if i % width != width - 1 {
+            res.push(i + 1);
+        }
+        if i >= width {
+            res.push(i - width);
+        }
+        if i < width * (width - 1) {
+            res.push(i + width);
+        }
+
+        // diagonals
+        if i % width != 0 && i >= width {
+            res.push(i - width - 1);
+        }
+        if i % width != width - 1 && i >= width {
+            res.push(i - width + 1);
+        }
+        if i % width != 0 && i < width * (width - 1) {
+            res.push(i + width - 1);
+        }
+        if i % width != width - 1 && i < width * (width - 1) {
+            res.push(i + width + 1);
+        }
+        res
+    })
+}
+
+pub fn solve_part1(input: &str) -> Result<String, anyhow::Error> {
+    let width = input.find('\n').unwrap();
+    let input = input.replace('\n', "");
+
+    let (numbers, symbols) = parse_input(input.as_str());
+
+    let adjacent_fn = adjacent(width);
+
+    let part_numbers = numbers
+        .iter()
+        .filter(|&number| {
+            symbols
+                .iter()
+                .flat_map(|&symbol| adjacent_fn(symbol))
+                .any(|adj| (number.start <= adj) && (adj <= number.end))
+        })
+        .map(|number| number.value)
+        .sum::<u32>();
+
+    Ok(format!("{}", part_numbers))
+}
+
+pub fn solve_part2(input: &str) -> Result<String, anyhow::Error> {
+    let width = input.find('\n').unwrap();
+    let input = input.replace('\n', "");
+
+    let (numbers, _) = parse_input(input.as_str());
+
+    let adjacent_fn = adjacent(width);
+
+    let gear_symbols = input
+        .chars()
+        .enumerate()
+        .filter_map(|(i, c)| {
+            let adjacents = adjacent_fn(i);
+
+            let adjacents_parts = numbers.iter().filter(|number| {
+                adjacents
+                    .iter()
+                    .any(|&adj| (number.start <= adj) && (adj <= number.end))
+            });
+
+            if c == '*' && adjacents_parts.count() == 2 {
+                Some(adjacents)
+            } else {
+                None
+            }
+        })
+        .map(|adjacents| {
+            numbers
+                .iter()
+                .filter(|&number| {
+                    adjacents
+                        .iter()
+                        .any(|&adj| (number.start <= adj) && (adj <= number.end))
+                })
+                .map(|number| number.value)
+                .product()
+        })
+        .collect::<Vec<u32>>();
+
+    Ok(format!("{}", gear_symbols.iter().sum::<u32>()))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_line() {
+        let input = "467.$.114*.";
+        let (numbers, symbols) = parse_input(input);
+
+        assert_eq!(numbers.len(), 2);
+
+        assert_eq!(
+            numbers[0],
+            Number {
+                value: 467,
+                start: 0,
+                end: 2
+            }
+        );
+        assert_eq!(
+            numbers[1],
+            Number {
+                value: 114,
+                start: 6,
+                end: 8
+            }
+        );
+        assert_eq!(symbols.len(), 2);
+
+        assert_eq!(symbols[0], 4);
+        assert_eq!(symbols[1], 9);
+    }
+}
