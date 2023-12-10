fdf4abc4a9b796b167e01510c51f0fa63fe2e0d9
Samu <el.tobal@gmail.com>
2023-12-10 19:54:28 +0100

    Day 10, 2023. Refactor part-1, simpler solution

--- 2023/day-10/src/part_1.rs
+++ 2023/day-10/src/part_1.rs
@@ -7,34 +7,13 @@
 use std::str::FromStr;
 
 use anyhow::Error;
+
 use itertools::Itertools;
-
-#[derive(PartialEq, Debug)]
-enum Pipe {
-    Vertical,
-    Horizontal,
-    NorthEast,
-    NorthWest,
-    SouthEast,
-    SouthWest,
-    Ground,
-    Start,
-}
-
-#[derive(Copy, Clone, Debug)]
-enum Direction {
-    North,
-    South,
-    East,
-    West,
-}
 
 #[derive(Debug)]
 struct Grid {
-    pipes: Vec<Pipe>,
+    pipes: Vec<char>,
     width: usize,
-    position: usize,
-    direction: Direction,
 }
 
 impl FromStr for Grid {
@@ -43,102 +22,85 @@ impl FromStr for Grid {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let width = s.lines().next().unwrap().len();
 
-        let s = s.replace('\n', "");
+        let pipes = s.replace('\n', "").chars().collect_vec();
 
-        let pipes = s
-            .chars()
-            .map(|c| match c {
-                '|' => Pipe::Vertical,
-                '-' => Pipe::Horizontal,
-                'L' => Pipe::NorthEast,
-                'J' => Pipe::NorthWest,
-                '7' => Pipe::SouthWest,
-                'F' => Pipe::SouthEast,
-                '.' => Pipe::Ground,
-                'S' => Pipe::Start,
-                _ => panic!("Invalid character"),
-            })
-            .collect_vec();
-
-        let position = s.find('S').unwrap();
-
-        let east = &pipes[position + 1];
-        let west = &pipes[position - 1];
-        let north = &pipes[position - width];
-        let south = &pipes[position + width];
-
-        let direction = if *east == Pipe::Horizontal
-            || *east == Pipe::NorthWest
-            || *east == Pipe::SouthWest
-        {
-            Direction::East
-        } else if *west == Pipe::Horizontal || *west == Pipe::NorthEast || *west == Pipe::SouthEast
-        {
-            Direction::West
-        } else if *north == Pipe::Vertical || *north == Pipe::SouthEast || *north == Pipe::SouthWest
-        {
-            Direction::North
-        } else if *south == Pipe::Vertical || *south == Pipe::NorthEast || *south == Pipe::NorthWest
-        {
-            Direction::South
-        } else {
-            panic!("Invalid start position");
-        };
-
-        Ok(Grid {
-            pipes,
-            width,
-            position,
-            direction,
-        })
+        Ok(Grid { pipes, width })
     }
 }
 
 #[derive(Debug)]
 struct State {
     position: usize,
-    direction: Direction,
+    direction: char,
 }
 
-impl Iterator for Grid {
-    type Item = State;
+impl Grid {
+    fn get_start(&self) -> State {
+        let position = self
+            .pipes
+            .iter()
+            .enumerate()
+            .find(|(_, &s)| s == 'S')
+            .unwrap()
+            .0;
 
-    fn next(&mut self) -> Option<Self::Item> {
-        let next_position = match self.direction {
-            Direction::North => self.position - self.width,
-            Direction::South => self.position + self.width,
-            Direction::East => self.position + 1,
-            Direction::West => self.position - 1,
+        let east = &self.pipes[position + 1];
+        let west = &self.pipes[position - 1];
+        let north = &self.pipes[position - self.width];
+        let south = &self.pipes[position + self.width];
+
+        let direction = if *east == '-' || *east == 'L' || *east == '7' {
+            'e'
+        } else if *west == '-' || *west == 'L' || *west == 'F' {
+            'w'
+        } else if *north == '|' || *north == 'F' || *north == '7' {
+            'n'
+        } else if *south == '|' || *south == 'L' || *south == 'J' {
+            's'
+        } else {
+            panic!("Invalid start position");
         };
 
-        let next_pipe = &self.pipes[next_position];
+        State {
+            position,
+            direction,
+        }
+    }
 
-        let next_direction = match (self.direction, next_pipe) {
-            (Direction::North, Pipe::Vertical) => Direction::North,
-            (Direction::North, Pipe::SouthEast) => Direction::East,
-            (Direction::North, Pipe::SouthWest) => Direction::West,
+    fn next_state(&self, state: &State) -> Option<State> {
+        let position = match state.direction {
+            'n' => state.position - self.width,
+            's' => state.position + self.width,
+            'e' => state.position + 1,
+            'w' => state.position - 1,
+            ch => unreachable!("Invalid direction {}", ch),
+        };
 
-            (Direction::South, Pipe::Vertical) => Direction::South,
-            (Direction::South, Pipe::NorthEast) => Direction::East,
-            (Direction::South, Pipe::NorthWest) => Direction::West,
+        let next_pipe = &self.pipes[position];
 
-            (Direction::East, Pipe::Horizontal) => Direction::East,
-            (Direction::East, Pipe::NorthWest) => Direction::North,
-            (Direction::East, Pipe::SouthWest) => Direction::South,
+        let direction = match (state.direction, next_pipe) {
+            ('n', '|') => 'n',
+            ('n', 'F') => 'e',
+            ('n', '7') => 'w',
 
-            (Direction::West, Pipe::Horizontal) => Direction::West,
-            (Direction::West, Pipe::NorthEast) => Direction::North,
-            (Direction::West, Pipe::SouthEast) => Direction::South,
+            ('s', '|') => 's',
+            ('s', 'L') => 'e',
+            ('s', 'J') => 'w',
+
+            ('e', '-') => 'e',
+            ('e', 'J') => 'n',
+            ('e', '7') => 's',
+
+            ('w', '-') => 'w',
+            ('w', 'L') => 'n',
+            ('w', 'F') => 's',
 
             (_, _) => return None,
         };
 
-        self.position = next_position;
-        self.direction = next_direction;
-
         Some(State {
-            position: self.position,
-            direction: self.direction,
+            position,
+            direction,
         })
     }
 }
@@ -146,9 +108,15 @@ impl Iterator for Grid {
 pub fn solve(input: &'static str) -> Result<String, anyhow::Error> {
     let grid = Grid::from_str(input)?;
 
-    let len = (grid.collect_vec().len() + 1) / 2;
+    let mut state = grid.get_start();
+    let mut len = 1;
 
-    Ok(len.to_string())
+    while let Some(next_state) = grid.next_state(&state) {
+        len += 1;
+        state = next_state;
+    }
+
+    Ok((len / 2).to_string())
 }
 
 #[cfg(test)]
@@ -156,26 +124,24 @@ mod tests {
     use super::*;
 
     const SAMPLE_1: &str = "-L|F7
- 7S-7|
- L|7||
- -L-J|
- L|-JF";
+7S-7|
+L|7||
+-L-J|
+L|-JF";
 
     const SAMPLE_2: &str = "..F7.
- .FJ|.
- SJ.L7
- |F--J
- LJ...";
+.FJ|.
+SJ.L7
+|F--J
+LJ...";
 
     #[test]
-    #[ignore = "not"]
     fn solve_sample1() {
         let result = solve(SAMPLE_1).unwrap();
         assert_eq!(result, "4");
     }
 
     #[test]
-    #[ignore = "reason"]
     fn solve_sample2() {
         let result = solve(SAMPLE_2).unwrap();
         assert_eq!(result, "8");
