12eac3664902e15fb60c22724f516273d820ca8a
Samu <el.tobal@gmail.com>
2023-12-23 20:33:54 +0100

    Day 23, 2023. Part2 optimized using fastforward when there is no decision to be made, bench time improved by ~1/100

--- 2023/day-23/src/part_2.rs
+++ 2023/day-23/src/part_2.rs
@@ -37,7 +37,7 @@ impl FromStr for Env {
             .flat_map(|(y, l)| {
                 l.chars()
                     .enumerate()
-                    .map(|(x, c)| {
+                    .filter_map(|(x, c)| {
                         let tile = match c {
                             '.' => Tile::Path,
                             '#' => Tile::Forest,
@@ -45,10 +45,10 @@ impl FromStr for Env {
                             '<' => Tile::Path,
                             '^' => Tile::Path,
                             'v' => Tile::Path,
-                            _ => panic!("invalid tile"),
+                            _ => return None,
                         };
 
-                        (IVec2::new(x as i32, y as i32), tile)
+                        Some((IVec2::new(x as i32, y as i32), tile))
                     })
                     .collect::<Vec<_>>()
             })
@@ -84,27 +84,73 @@ impl Display for Env {
     }
 }
 
-const DIRECTIONS: &[IVec2; 4] = &[IVec2::X, IVec2::Y, IVec2::NEG_X, IVec2::NEG_Y];
+impl Env {
+    fn adjacents(&self, p: &IVec2) -> Vec<(IVec2, usize)> {
+        [IVec2::X, IVec2::Y, IVec2::NEG_X, IVec2::NEG_Y]
+            .iter()
+            .filter_map(|&d| {
+                let q = *p + d;
+
+                match self.tiles.get(&q) {
+                    Some(Path) => Some((q, 1)),
+                    _ => None,
+                }
+            })
+            .collect()
+    }
+
+    fn fast_forward(&self, p: &IVec2, d: &IVec2) -> (IVec2, usize) {
+        let mut p = *p;
+        let mut d = *d;
+        let mut dist = 0;
+
+        loop {
+            let adjacents: Vec<_> = [IVec2::X, IVec2::Y, IVec2::NEG_X, IVec2::NEG_Y]
+                .iter()
+                .filter_map(|&q_d| {
+                    if (q_d + d) == IVec2::ZERO {
+                        return None;
+                    }
+
+                    let q = p + q_d;
+
+                    match self.tiles.get(&q) {
+                        Some(Path) => Some((q, q_d, dist + 1)),
+                        _ => None,
+                    }
+                })
+                .collect();
+
+            if adjacents.len() == 1 {
+                (p, d, dist) = *adjacents.first().unwrap();
+            } else {
+                break (p, dist);
+            }
+        }
+    }
+
+    fn adjacents_fast_forwarded(&self, p: &IVec2) -> Vec<(IVec2, usize)> {
+        [IVec2::X, IVec2::Y, IVec2::NEG_X, IVec2::NEG_Y]
+            .iter()
+            .filter_map(|&d| {
+                let q = *p + d;
+
+                match self.tiles.get(&q) {
+                    Some(Path) => {
+                        Some((self.fast_forward(&q, &d).0, self.fast_forward(&q, &d).1 + 1))
+                    }
+                    _ => None,
+                }
+            })
+            .collect()
+    }
+}
 
 pub fn solve(input: &'static str) -> String {
     let env: Env = input.parse().unwrap();
 
     let s = IVec2::new(1, 0);
     let g = IVec2::new(env.size.x - 2, env.size.y - 1);
-
-    let generate = move |p| -> Vec<(_, _)> {
-        DIRECTIONS
-            .iter()
-            .filter_map(|&d| {
-                let q = p + d;
-
-                match env.tiles.get(&q) {
-                    Some(Path) => Some((q, 1)),
-                    _ => None,
-                }
-            })
-            .collect()
-    };
 
     let mut queue = VecDeque::new();
     queue.push_front(s);
@@ -115,18 +161,18 @@ pub fn solve(input: &'static str) -> String {
 
     while let Some(p) = queue.pop_back() {
         if !visited.contains(&p) {
-            for (q, w) in generate(p) {
-                let n = match graph.node_indices().find(|i| graph[*i] == p) {
-                    Some(n) => n,
-                    None => graph.add_node(p),
-                };
+            for (q, d) in env.adjacents_fast_forwarded(&p) {
+                let n = graph
+                    .node_indices()
+                    .find(|i| graph[*i] == p)
+                    .unwrap_or_else(|| graph.add_node(p));
 
-                let m = match graph.node_indices().find(|i| graph[*i] == q) {
-                    Some(m) => m,
-                    None => graph.add_node(q),
-                };
+                let m = graph
+                    .node_indices()
+                    .find(|i| graph[*i] == q)
+                    .unwrap_or_else(|| graph.add_node(q));
 
-                graph.add_edge(n, m, w);
+                graph.add_edge(n, m, d);
 
                 queue.push_back(q);
             }
@@ -140,7 +186,23 @@ pub fn solve(input: &'static str) -> String {
 
     let ways = algo::all_simple_paths::<Vec<_>, _>(&graph, s, g, 0, None).collect::<Vec<_>>();
 
-    (ways.iter().map(|w| w.len()).max().unwrap() - 1).to_string()
+    (ways
+        .iter()
+        .map(|w| {
+            let mut dist = 0;
+
+            for i in 0..w.len() - 1 {
+                let n = w[i];
+                let m = w[i + 1];
+
+                dist += graph.edge_weight(graph.find_edge(n, m).unwrap()).unwrap();
+            }
+
+            dist
+        })
+        .max()
+        .unwrap())
+    .to_string()
 }
 
 #[cfg(test)]
@@ -153,5 +215,53 @@ mod tests {
     fn solve_sample() {
         let result = solve(SAMPLE);
         assert_eq!(result, "154");
+    }
+
+    #[test]
+    fn fast_forward_test() {
+        let input = r"#########
+#.......#
+#########";
+
+        let env = input.parse::<Env>().unwrap();
+
+        let p = IVec2::new(1, 1);
+
+        let adjacents = env.adjacents(&p);
+
+        assert_eq!(adjacents.len(), 1);
+    }
+
+    #[test]
+    fn fast_forward_test_2() {
+        let input = r"#.#######
+#....####
+####....#
+####.##.#
+#######.#";
+
+        let env = input.parse::<Env>().unwrap();
+
+        let p = IVec2::new(1, 0);
+
+        let adjacents = env.adjacents_fast_forwarded(&p);
+
+        assert_eq!(adjacents, vec![(IVec2::new(4, 2), 5)]);
+    }
+
+    #[test]
+    fn fast_forward_test_3() {
+        let input = r"#.#######
+#....####
+####....#
+#######.#";
+
+        let env = input.parse::<Env>().unwrap();
+
+        let p = IVec2::new(1, 0);
+
+        let adjacents = env.adjacents_fast_forwarded(&p);
+
+        assert_eq!(adjacents, vec![(IVec2::new(7, 3), 9)]);
     }
 }
